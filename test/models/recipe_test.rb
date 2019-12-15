06f54e56a4081fabcda7976ce6007f04639c9a3d
Matthew Schultz <abstractapproach@gmail.com>
2019-12-15 16:46:23 -0600

    Add recipe model tests

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot have recipe without title" do
+    recipe = Recipe.new
+    assert_not recipe.save
+    assert_includes recipe.errors, :title
+  end
+  test "cannot have recipe without cusine" do
+    recipe = Recipe.new
+    assert_not recipe.save
+    assert_includes recipe.errors, :cusine
+  end
+  test "cannot have recipe without instructions" do
+    recipe = Recipe.new
+    assert_not recipe.save
+    assert_includes recipe.errors, :instructions
+  end
+  test "cannot have recipe without tried_before" do
+    recipe = Recipe.new
+    assert_not recipe.save
+    assert_includes recipe.errors, :tried_before
+  end
 end
