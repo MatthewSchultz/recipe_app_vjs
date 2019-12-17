3b9001758c88e02add249539245d9dc8c85d37f2
Matthew Schultz <abstractapproach@gmail.com>
2019-12-17 15:47:06 -0600

    Add test for #10.

--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -30,6 +30,57 @@ class RecipesTest < ApplicationSystemTestCase
     assert_selector 'tr.recipe td:nth-child(1)', text: 'New Title'
   end
 
+  # ISSUE GH-10
+  test 'changing an ingredient' do
+    # Create a duplicate recipe to mess about with
+    r = Recipe.first
+    r2 = r.dup
+    r2.title = 'New duplicate recipe'
+    r2.save
+
+    # Fill it with the same ingredients
+    r.recipe_ingrediants.each do |ri|
+      r2.recipe_ingrediants.create(
+        ingrediant: ri.ingrediant,
+        qty: ri.qty,
+        unit: ri.unit
+      )
+    end
+    i = r.ingrediants.first
+    i_original_name = i.name
+
+    # Goto recipes page
+    visit recipes_url
+
+    # Find the recipe
+    within "tr.recipe[data-uuid='#{r2.id}']" do
+      # Check that the second recipe was created:
+      assert_selector 'td:nth-child(1)', text: r2.title
+
+      # Make sure it has the ingredient:
+      assert_selector "td:nth-child(6) li", text: i.name
+
+      # Go to the edit page of the recipe:
+      click_on 'Edit'
+    end
+
+    # Change the ingrediant:
+    fill_in currently_with: i_original_name, with: 'New Ingredient'
+    click_on 'Save'
+    assert_text "Recipe was successfully updated"
+
+    # Check the new recipe:
+    within "tr.recipe[data-uuid='#{r2.id}']" do
+      assert_selector "tr.recipe[data-uuid='#{r2.id}'] td:nth-child(6) li", text: 'New Ingredient'
+    end
+
+    # Check the old recipe:
+    within "tr.recipe[data-uuid='#{r.id}']" do
+      assert_selector "tr.recipe[data-uuid='#{r2.id}'] td:nth-child(6) li", text: i_original_name
+    end
+  end
+
+
   test "destroying a Recipe" do
     visit recipes_url
     page.accept_confirm do
