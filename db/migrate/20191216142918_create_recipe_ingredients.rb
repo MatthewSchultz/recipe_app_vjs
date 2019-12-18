8155f2fbf9d477ef926752d4fc8e3832247acd21
Matthew Schultz <abstractapproach@gmail.com>
2019-12-17 18:56:08 -0600

    finish

--- db/migrate/20191216142918_create_recipe_ingredients.rb
+++ db/migrate/20191216142918_create_recipe_ingredients.rb
@@ -1,8 +1,8 @@
-class CreateRecipeIngrediants < ActiveRecord::Migration[6.0]
+class CreateRecipeIngredients < ActiveRecord::Migration[6.0]
   def change
-    create_table :recipe_ingrediants, id: :uuid do |t|
+    create_table :recipe_ingredients, id: :uuid do |t|
       t.references :recipe, null: false, foreign_key: true, type: :uuid
-      t.references :ingrediant, null: false, foreign_key: true, type: :uuid
+      t.references :ingredient, null: false, foreign_key: true, type: :uuid
       t.decimal :qty, null: false, precision: 3
       t.integer :unit, null: false, limit: 1
 
--- test/models/recipe_ingredient_test.rb
+++ test/models/recipe_ingredient_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RecipeIngrediantTest < ActiveSupport::TestCase
+class RecipeIngredientTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
